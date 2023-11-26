a6a47b6fa5029e44edc19eef0933c710d544b00e
umaidashi <umaidashi18@gmail.com>
2023-11-26 17:27:17 +0900

    feat: 妥協点

--- src/extension.ts
+++ src/extension.ts
@@ -1,36 +1,34 @@
 import * as vscode from "vscode";
 
-// export function activate(context: vscode.ExtensionContext) {
-//   let disposable = vscode.commands.registerCommand(
-//     "test.command",
-//     (args) => {
-//       const msext = vscode.extensions.getExtension("Github.copilot");
-//       if (!msext) {
-//         return;
-//       }
-//       msext?.activate().then(() => {
-//         vscode.commands
-//           .executeCommand("editor.action.inlineSuggest.commit", args)
-//           .then((res) => {
-//             console.log(res);
-//           });
-//       });
-//       vscode.authentication
-//         .getSession("github", ["user:name"], {
-//           createIfNone: true,
-//         })
-//         .then((res) => {
-//           vscode.window.showInformationMessage(
-//             `${msext ? "copilot is installed" : "copilot is not installed"} ${
-//               res.account.label
-//             }}`
-//           );
-//         });
-//     }
-//   );
+export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand("test.command", (args) => {
+    const msext = vscode.extensions.getExtension("Github.copilot");
+    if (!msext) {
+      return;
+    }
+    msext?.activate().then(() => {
+      vscode.commands
+        .executeCommand("editor.action.inlineSuggest.commit", args)
+        .then((res) => {
+          console.log(args, res);
+          vscode.window.showInformationMessage("github copilotを使用しました");
+        });
+    });
+    vscode.authentication
+      .getSession("github", ["user:name"], {
+        createIfNone: true,
+      })
+      .then((res) => {
+        vscode.window.showInformationMessage(
+          `${msext ? "認証しました" : "認証できませんでした"} ${
+            res.account.label
+          }`
+        );
+      });
+  });
 
-//   context.subscriptions.push(disposable);
-// }
+  context.subscriptions.push(disposable);
+}
 
 // exports.activate = function () {
 //   console.log(`I am active! copilot`);
@@ -49,7 +47,7 @@ import * as vscode from "vscode";
 //           vscode.window.showInformationMessage(
 //             `${msext ? "copilot is installed" : "copilot is not installed"} ${
 //               res.account.label
-//             }}`
+//             }`
 //           );
 //         });
 //       msext?.activate().then(() => {
@@ -60,23 +58,6 @@ import * as vscode from "vscode";
 //       });
 //     }
 //   );
-// };
-
-exports.activate = function () {
-  console.log(`I am active!`);
-  vscode.commands.registerCommand("editor.action.inlineSuggest.commit", () => {
-    console.log(`type with args`);
-    return vscode.commands.executeCommand(
-      "editor.action.inlineSuggest.commit"  
-    );
-  });
-};
-// exports.activate = function () {
-//   console.log(`I am active!`);
-//   vscode.commands.registerCommand("type", (args) => {
-//     console.log(`type with args`, args);
-//     return vscode.commands.executeCommand("default:type", args);
-//   });
 // };
 
 export function deactivate() {}
